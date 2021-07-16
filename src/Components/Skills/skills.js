e6a1889d9d08fa241e5de6f1e1301a0e9c1c8721
Izzy <itzisraelone@gmail.com>
2021-07-16 15:08:25 -0500

    Created new component for Skills Section

--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -1,20 +1,83 @@
-import React, { useReact } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: blue;
+  display: flex;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+`;
 
-const IconContainer = styled.div``;
+const SkillContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+`;
 
-const Icon = styled.i``;
+const IconContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.i`
+  color: blue;
+  font-size: 25px;
+`;
 
 const Header = styled.h1`
   text-align: center;
+  color: white;
+`;
+
+const SkillHeader = styled.h4`
+  color: white;
+  margin: 0;
+`;
+
+const Description = styled.p`
+  color: white;
+  margin: 0;
 `;
 
 const Skills = () => {
   return (
     <Container>
       <Header>Skills</Header>
+      <SkillContainer>
+        <IconContainer>
+          <Icon className="fas fa-code"></Icon>
+        </IconContainer>
+        <SkillHeader>Technologies</SkillHeader>
+        <Description>Html, CSS, Javascript, React</Description>
+      </SkillContainer>
+      <SkillContainer>
+        <IconContainer>
+          <Icon className="fas fa-code"></Icon>
+        </IconContainer>
+        <SkillHeader>Technologies</SkillHeader>
+        <Description>Html, CSS, Javascript, React</Description>
+      </SkillContainer>
+      <SkillContainer>
+        <IconContainer>
+          <Icon className="fas fa-code"></Icon>
+        </IconContainer>
+        <SkillHeader>Technologies</SkillHeader>
+        <Description>Html, CSS, Javascript, React</Description>
+      </SkillContainer>
     </Container>
   );
 };

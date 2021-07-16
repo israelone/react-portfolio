87ac7abce8ebc7ac6d7284ebf5be8b1e7225659f
Izzy <itzisraelone@gmail.com>
2021-07-16 11:16:05 -0500

    Created Navigation Bar and its styling

---
+++ src/Components/Skills/skills.js
@@ -0,0 +1,22 @@
+import React, { useReact } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const IconContainer = styled.div``;
+
+const Icon = styled.i``;
+
+const Header = styled.h1`
+  text-align: center;
+`;
+
+const Skills = () => {
+  return (
+    <Container>
+      <Header>Skills</Header>
+    </Container>
+  );
+};
+
+export default Skills;

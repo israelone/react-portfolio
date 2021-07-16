33bdddffdd1fa5024baa668e9a2067d25f17bfb6
Izzy <itzisraelone@gmail.com>
2021-07-16 15:16:04 -0500

    Moved Projects Component into Component Folder and changed styling to match template

---
+++ src/Components/Projects/Project/project.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProjectContainer = styled.div`
+  border: 2px solid black;
+  width: 80%;
+  height: 300px;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 20px;
+`;
+
+const ProjectTitle = styled.h5`
+  margin: 0;
+  padding-left: 30px;
+`;
+
+const ProjectSummary = styled.p`
+  padding-left: 30px;
+  margin: 5px 0 25px 0;
+`;
+
+const Project = () => {
+  return (
+    <ProjectContainer>
+      <ProjectTitle>Shopping Cart</ProjectTitle>
+      <ProjectSummary>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </ProjectSummary>
+    </ProjectContainer>
+  );
+};
+
+export default Project;

ef287821d6b8b30f84dfef769ca5fbe78ee1a268
Izzy <itzisraelone@gmail.com>
2021-07-20 11:04:50 -0500

    Added new elements and styling

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: #fffffe;
+  padding: 80px 20px;
+`;
+
+const Header = styled.h1``;
+const Introduction = styled.p`
+  color: #5f6c7b;
+`;
 
 const About = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header>Hello!</Header>
+      <Introduction>
+        Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+      </Introduction>
+    </Container>
+  );
 };
 
 export default About;

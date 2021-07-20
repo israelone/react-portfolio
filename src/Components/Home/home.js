0da16803be1b6af39cd0f51b416b18f7d697a537
Izzy <itzisraelone@gmail.com>
2021-07-20 15:02:00 -0500

    Added gradient background and added more styling

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: #c1c14b;
+  font-size: 25px;
+  background: linear-gradient(
+    125deg,
+    #ecfcff 0%,
+    #ecfcff 40%,
+    #b2fcff calc(40% + 1px),
+    #b2fcff 60%,
+    #5edfff calc(60% + 1px),
+    #5edfff 72%,
+    #3e64ff calc(72% + 1px),
+    #3e64ff 100%
+  );
+  ${'' /* background-color: #c1c14b; */}
   height: 88vh;
   display: flex;
   flex-direction: column;
@@ -14,16 +26,26 @@ const Container = styled.div`
 
 const Paragraph = styled.p`
   font-style: italic;
+  margin: 0;
+  font-weigth: 300;
+  font-size: 18px;
 `;
 
 const Header = styled.h1`
   margin: 0;
+  letter-spacing: 7px;
+  font-family: 'Teko', sans-serif;
+  color: #094067;
 `;
 
 const Button = styled.button`
-  border: none;
-  padding: 22px;
-  border-radius: 3px;
+  margin: 17px 0;
+  border: 2px solid #094067;
+  padding: 17px;
+  font-size: 20px;
+  background-color: #fffffe;
+  color: #094067;
+  border-radius: 5px;
 `;
 
 const Home = () => {
@@ -31,7 +53,7 @@ const Home = () => {
     <Container>
       <Header>Israel Lopez</Header>
       <Paragraph>Front End Developer</Paragraph>
-      <Button>Find Out More</Button>
+      <Button>About Me</Button>
     </Container>
   );
 };

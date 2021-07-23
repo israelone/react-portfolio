cdeb3474d0260caf8295eca4517166c5a4dfa589
Izzy <itzisraelone@gmail.com>
2021-07-23 16:11:46 -0500

    Added smooth scrolling when a link is clicked

--- src/App.js
+++ src/App.js
@@ -46,6 +46,17 @@ const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth',
+        });
+      });
+    });
+  }, []);
+
   return (
     <>
       <MenuButton

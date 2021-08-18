3f599c0eb8b5d3787364532e45483cd645ae6c85
Izzy <itzisraelone@gmail.com>
2021-08-17 22:37:39 -0500

    Remove border, and added dynamic background-color styling based on props passed to component

--- src/Components/Projects/Project/project.js
+++ src/Components/Projects/Project/project.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProjectContainer = styled.div`
-  border: 2px solid black;
+  color: white;
+
   width: 80%;
   height: 300px;
   margin: 20px auto;
@@ -10,7 +11,14 @@ const ProjectContainer = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   padding: 20px;
-  background-color: #fffffe;
+  background-color: ${(props) =>
+    props.backgroundcolor === 'darkblue'
+      ? '#2b8cb0'
+      : props.backgroundcolor === 'blue'
+      ? '#4c61fc'
+      : props.backgroundcolor === 'green'
+      ? '#3dfc7b'
+      : '#2bb056'};
 
   @media only screen and (min-width: 1000px) {
     width: initial;
@@ -19,7 +27,7 @@ const ProjectContainer = styled.div`
     animation: scale-down-center 0.5s 1;
     &:hover {
       -webkit-transform: scale(0.5);
-      transform: scale(0.8);
+      transform: scale(0.9);
     }
   }
 `;
@@ -34,9 +42,10 @@ const ProjectSummary = styled.p`
   margin: 5px 0 25px 0;
 `;
 
-const Project = () => {
+const Project = (props) => {
+  console.log(props.color);
   return (
-    <ProjectContainer>
+    <ProjectContainer backgroundcolor={props.color}>
       <ProjectTitle>Shopping Cart</ProjectTitle>
       <ProjectSummary>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod

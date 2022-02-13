c313807fb04f7eae9503f6f116e0f70b8bfb09e3
IzzyDev <itzisraelone@gmail.com>
2022-02-13 09:46:46 -0600

    Fixed the way the project gets rendered

--- src/Components/Projects/Project/project.js
+++ src/Components/Projects/Project/project.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Image from "../../../resources/projectsImages/e-commerce.jpg";
 
+const ProjectTitle = styled.h5`
+  margin: 0;
+  position: relative;
+  z-index: 10;
+  transition: visibility ease 0.2s;
+  @media only screen and (min-width: 1000px) {
+    display: none;
+  }
+`;
+
+const ProjectSummary = styled.p`
+  margin: 5px 0 25px 0;
+  @media only screen and (min-width: 1000px) {
+    visibility: hidden;
+  }
+`;
+
+const BackgroundImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 5;
+  height: 220px;
+`;
 const ProjectContainer = styled.div`
+padding: 0 10px;
+position: relative;
+  background-position:center;
+  background-size:100%;
   color: white;
-
   width: 80%;
   height: 300px;
   margin: 20px auto;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  padding: 20px;
   background-color: ${(props) =>
-    props.backgroundcolor === 'darkblue'
-      ? '#2b8cb0'
-      : props.backgroundcolor === 'blue'
-      ? '#4c61fc'
-      : props.backgroundcolor === 'green'
-      ? '#3dfc7b'
-      : '#2bb056'};
+    props.backgroundcolor === "darkblue"
+      ? "#2b8cb0"
+      : props.backgroundcolor === "blue"
+      ? "#4c61fc"
+      : props.backgroundcolor === "green"
+      ? "#3dfc7b"
+      : "#2bb056"};
 
+ 
   @media only screen and (min-width: 1000px) {
     width: initial;
     margin: 0;
@@ -28,29 +57,33 @@ const ProjectContainer = styled.div`
     &:hover {
       -webkit-transform: scale(0.5);
       transform: scale(0.9);
+   
+    ${BackgroundImage}{
+      opacity: 0.3;
     }
+    ${ProjectTitle}, ${ProjectSummary}{
+      display:initial;
+      visibility: visible;
+      transition: visibility ease 0.2s;
+    
+  }
   }
 `;
 
-const ProjectTitle = styled.h5`
-  margin: 0;
-  padding-left: 30px;
-`;
-
-const ProjectSummary = styled.p`
-  padding-left: 30px;
-  margin: 5px 0 25px 0;
-`;
-
 const Project = (props) => {
-  console.log(props.color);
+  console.log(props.projectImage);
   return (
-    <ProjectContainer backgroundcolor={props.color}>
-      <ProjectTitle>Shopping Cart</ProjectTitle>
-      <ProjectSummary>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua
-      </ProjectSummary>
+    <ProjectContainer className="project-tile" backgroundcolor={props.color}>
+      <BackgroundImage src={Image} alt="" />
+      <ProjectTitle>
+        <a href="#" target="_blank">
+          E-Commerce
+        </a>{" "}
+        <a href="https://github.com/israelone/e-commerce" target="_blank">
+          Github
+        </a>
+      </ProjectTitle>
+      <ProjectSummary>E-Commerce page created using AngularJs</ProjectSummary>
     </ProjectContainer>
   );
 };
